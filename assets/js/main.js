847940d6e07ebcb9306f1a12eefe9a33f35ab618
FrontJSack <72698651+FrontJSack@users.noreply.github.com>
2021-11-18 04:25:57 +0100

    1.0.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -128,12 +128,12 @@
   /**
    * Preloader
    */
-  let preloader = select('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove()
-    });
-  }
+   let preloader = select('#preloader');
+   if (preloader) {
+     window.addEventListener('load', () => {
+       preloader.remove()
+     });
+   }
 
   /**
    * Hero type effect
